bd3cb4195513bd64322ea394005b1673b0ad3bfc
jmarusak <jmarusak@users.noreply.github.com>
2024-03-31 09:58:20 -0400

    scala scripts have extension sc

--- scripts/charcount.scala
+++
@@ -1,7 +0,0 @@
-val str: String = "hello"
-
-//val map = str.groupBy(identity).view.mapValues(_.size).toMap
-
-var counts: Map[Char, Int] = Map.empty[Char, Int]
-
-println(counts)
--- scripts/patternMatching.scala
+++
@@ -1,8 +0,0 @@
-val x = List(0, 1, 2)
-
-val ismatch = x match {
-  case List(_,_,2) => true
-  case List(_,_) => false
-}
-
-print(ismatch)
